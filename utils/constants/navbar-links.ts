acde9c6f4fae9b6a64e3474f6330a3b4e5b8d414
abdulrahman <kurdistanhere20@gmail.com>
2025-11-03 18:52:52 +0300

    Add navbar links constants for application navigation

---
+++ utils/constants/navbar-links.ts
@@ -0,0 +1,6 @@
+export const navbarLinks = [
+  { name: "Films", path: "/films" },
+  { name: "Lists", path: "/lists" },
+  { name: "Members", path: "/members" },
+  { name: "Journal", path: "/journal" },
+];
