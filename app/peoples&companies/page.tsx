8b45236cdba282efcfe71f7c3105b87ddf317e81
abdulrahman <kurdistanhere20@gmail.com>
2025-11-26 01:41:52 +0300

    feat: add PeopleAndCompaniesPage component and create page wrapper

---
+++ app/peoples&companies/page.tsx
@@ -0,0 +1,12 @@
+import PeopleAndCompaniesPage from "@/routes/people&companies";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <PeopleAndCompaniesPage />
+    </div>
+  );
+};
+
+export default page;
