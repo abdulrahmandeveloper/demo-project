d357db5a02fd314e92f53d131357a8226b9eda85
abdulrahman <kurdistanhere20@gmail.com>
2025-11-26 19:35:24 +0300

    feat: enhance MoviePage with detailed movie display; add genre listing and backdrop image

--- core/features/movie/interfaces/tmdb.interface.ts
+++ core/features/movie/interfaces/tmdb.interface.ts
@@ -1,8 +1,12 @@
-import { TMDBCommonMediaDataResponse } from "@/shared/interfaces/tmdb.interface";
+import {
+  Genres,
+  TMDBCommonMediaDataResponse,
+} from "@/shared/interfaces/tmdb.interface";
 
 export interface TMDBMovieResponse extends TMDBCommonMediaDataResponse {
   original_title: string;
   release_date: string;
+  genres: Genres[];
   title: string;
   video: boolean;
 }
--- core/routes/movies/movieId/index.tsx
+++ core/routes/movies/movieId/index.tsx
@@ -11,6 +11,7 @@ const MoviePage = () => {
   const [movie, setMovie] = useState<TMDBMovieResponse>(
     {} as TMDBMovieResponse
   );
+  console.log(movie.genre_ids);
 
   const params = useParams();
 
@@ -37,7 +38,56 @@ const MoviePage = () => {
           search={true}
         />
       </div>
-      <div className="dark:text-white text-5xl ">{movie.title}</div>
+      <div className="dark:text-white text-5xl ">
+        <div className="h-[50vh] ">
+          <img
+            src={`${process.env.NEXT_PUBLIC_IMAGES_BASE_URL}/${movie.backdrop_path}`}
+            alt={movie.title}
+            className="absolute top-0 -z-50 opacity-75 h-[30vh] w-full object-cover object-center "
+          />
+          <div className="flex h-[55vh] mt-28 ">
+            <img
+              src={`${process.env.NEXT_PUBLIC_IMAGES_BASE_URL}/${movie.poster_path}`}
+              alt={movie.title}
+              className=" h-full ml-10 rounded-lg shadow-lg    top-0"
+            />
+            <div className="flex  w-1/2 ">
+              <div className="justify-center flex flex-col mx-10 mb-4/5 ">
+                <div className="">
+                  <div className="flex items-center gap-2 mb-5">
+                    {" "}
+                    <h1 className="text-4xl font-bold">{movie.title}</h1>{" "}
+                    <p className="text-sm text-center mx-2 mt-5 bg-green-500 py-px px-0.5 text-black rounded-sm">
+                      {movie.vote_average}
+                    </p>{" "}
+                  </div>
+                  <p className="text-lg font-sm ">{movie.overview}</p>
+                  <div className="flex gap-4 mt-10">
+                    <p className="text-3xl text-center flex my-auto">
+                      {" "}
+                      Genres:
+                    </p>
+                    {movie?.genres?.map((genre, index) => (
+                      <div
+                        className="flex items-center justify-center"
+                        key={index}
+                      >
+                        <p className="text-sm flex gap-4   mt-auto items-end border border-white py-0.5 px-1 opacity-60 rounded-md">
+                          {genre.name}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center items-center w-full">
+                <h1 className="">Trailer</h1>
+                <div className="w-full h-[10vh] border-amber-300  border"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
